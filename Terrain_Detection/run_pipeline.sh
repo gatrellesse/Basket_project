ef3af075592e0622f22071285f26b8d7c030ffa9
gatrellesse <gabrielbtrellesse@gmail.com>
2025-05-02 15:58:43 +0200

    fix: new path

--- Terrain_Detection/run_pipeline.sh
+++ Terrain_Detection/run_pipeline.sh
@@ -5,7 +5,7 @@ python3 src/pos_processing/collinear.py \
   --config src/prediction/superpoint_config.json
 
 echo "Running superpointREF.py with config..."
-python3 src/prediction/superpointREF_Inter.py \
+python3 src/prediction/superpointREF.py \
   --config src/prediction/superpoint_config.json
 
-echo "Pipeline complete."
+echo "Terrain Dection complete."
